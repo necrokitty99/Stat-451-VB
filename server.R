58eb71a6c218fe61d037994739bfbf860e140daa
lindhcar000 <103108047+lindhcar000@users.noreply.github.com>
2024-11-23 17:54:44 -0800

    Update server.R

--- server.R
+++ server.R
@@ -9,26 +9,43 @@ library(plotly)
 library(shiny)
 
 #opponent attack type data
-attack_data <- data.frame(
+attack_data_2023 <- data.frame(
   Play = c("Go", "Red", "Hut", "Mak", "Quick", "Gap", "Slide", "Bic", "Total"),
   ATT = c(895, 457, 593, 163, 235, 210, 161, 99, 3341),
-  K_Percent = c("38%", "42%", "25%", "22%", "51%", "42%", "42%", "35%", "36%"),
-  E_Percent = c("9%", "7%", "12%", "15%", "5%", "6%", "12%", "17%", "9%"),
-  B_Percent = c("6%", "5%", "6%", "6%", "5%", "5%", "7%", "4%", "6%"),
+  K_Percent = c(38, 42, 25, 22, 51, 42, 42, 35, 36),
+  E_Percent = c(9, 7, 12, 15, 5, 6, 12, 17, 9),
+  B_Percent = c(6, 5, 6, 6, 5, 5, 7, 4, 6),
   Eff = c(22.9, 30.4, 7.6, 1.8, 41.7, 31.4, 23.6, 14.1, 21.6),
   FBSO = c(0.222, 0.299, 0.075, -0.075, 0.408, 0.308, 0.206, 0.186, 0.222),
   Trans = c(0.237, 0.312, 0.076, 0.049, 0.430, 0.323, 0.296, 0.107, 0.210)
 )
 
-#opponent attack type transformations
-attack_data$B_Percent <- as.numeric(sub("%", "", attack_data$B_Percent))
+attack_data_2023 <- attack_data[attack_data$Play != "Total", ]
 
-attack_data$K_Percent <- as.numeric(sub("%", "", attack_data$K_Percent))
+attack_data_2023 <- attack_data %>%
+  mutate(Play = fct_reorder(Play, K_Percent, .desc = TRUE))
 
-attack_data <- attack_data[attack_data$Play != "Total", ]
+attack_data_long2_2023 <- melt(attack_data, id.vars = "Play", measure.vars = c("K_Percent", "Eff", "B_Percent"),
+                          variable.name = "Metric", value.name = "Value") 
 
-attack_data_long2 <- melt(attack_data, id.vars = "Play", measure.vars = c("K_Percent", "Eff", "B_Percent"),
-                          variable.name = "Metric", value.name = "Value")
+attacking_data_2024 <- data.frame(
+  Play = c("Go", "Red", "Hut", "Mak", "Quick", "Gap", "Slide", "Bic"),
+  ATT = c(580, 263, 218, 55, 132, 113, 159, 62),
+  K_Percent = c(32, 40, 24, 33, 37, 43, 42, 27),
+  E_Percent = c(9, 11, 7, 7, 7, 6, 6, 16),
+  B_Percent = c(10, 8, 8, 5, 3, 4, 5, 3),
+  Eff = c(0.129, 0.198, 0.087, 0.200, 0.273, 0.327, 0.314, 0.081),
+  FBSO = c(0.197, 0.204, 0.103, 0.444, 0.189, 0.333, 0.316, 0.138),
+  Trans = c(0.049, 0.188, 0.081, 0.152, 0.452, 0.319, 0.311, 0.030)
+)
+
+attack_data_2024 <- attack_data_2024[attack_data_2024$Play != "Total", ]
+
+attack_data_2024 <- attack_data_2024 %>%
+  mutate(Play = fct_reorder(Play, K_Percent, .desc = TRUE))
+
+attack_data_long2_2024 <- melt(attack_data_2024, id.vars = "Play", measure.vars = c("K_Percent", "Eff", "B_Percent"),
+                               variable.name = "Metric", value.name = "Value") 
 
 
 # Chart 1/2 Data
@@ -104,15 +121,29 @@ function(input, output) {
     }
   })
   # Chart 2 Code
-  output$opponent_data <- renderPlot({
-    ggplot(attack_data_long2, aes(x = Play, y = Value, fill = Metric)) +
+  output$opponent_data <- renderPlotly({
+    if(input$year == "2023"){
+    ggplot(attack_data_long_2023, aes(x = Play, y = Value, fill = Metric)) +
       geom_bar(stat = "identity", position = position_dodge()) +
-      labs(title = "What plays are UW volleyball weak against?", x = "Play Type", y = "Kills / Efficiency / Blocks (%)") +
-      scale_fill_manual(values = c("skyblue", "orange", "purple"), labels = c("Kills (%)", "Efficiency (%)", "Blocks (%)")) +
+      labs(title = "What plays are UW volleyball weak against in 2023?", 
+           x = "Play Type", 
+           y = "Kills / Efficiency / Blocks (%)") +
+      scale_fill_manual(values = c("skyblue", "orange", "purple"), 
+                        labels = c("Kills (%)", "Efficiency (%)", "Blocks (%)")) +
       theme_light() +
       theme(legend.title = element_blank())
-  })
-
+    }}
+  else if (input$year =="2024"){
+    ggplot(attack_data_long_2024, aes(x = Play, y = Value, fill = Metric)) +
+      geom_bar(stat = "identity", position = position_dodge()) +
+      labs(title = "What plays are UW volleyball weak against in 2024?", 
+           x = "Play Type", 
+           y = "Kills / Efficiency / Blocks (%)") +
+      scale_fill_manual(values = c("skyblue", "orange", "purple"), 
+                        labels = c("Kills (%)", "Efficiency (%)", "Blocks (%)")) +
+      theme_light() +
+      theme(legend.title = element_blank())
+  }
   # Chart 3 Code
   output$WinLoss <- renderPlot({
     plot1 <- ggplot() + 
